58da98a3f1171d131032e3635fa4f6b22e436db3
steve.bement <steve.bement@accenture.com>
2021-11-11 09:10:53 -0700

    Initial commit

---
+++ GameOfLife.Tests.Unit/GameOfLifeTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace GameOfLife.Tests.Unit
+{
+    public class GameOfLifeTests
+    {
+        [Fact]
+        public void CreatingGrid_DimensionsAreCorrect()
+        {
+            Grid grid = new Grid(10, 10);
+
+            grid.Width.Should().Be(10);
+            grid.Height.Should().Be(9);
+        }
+    }
+}
---
+++ GameOfLife/Grid.cs
@@ -0,0 +1,14 @@
+﻿namespace GameOfLife
+{
+    public class Grid
+    {
+        public Grid(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width { get; }
+        public int Height { get; set; }
+    }
+}
